1807126e8136f5718ba7f7c4c1f0e9ed0b37ad35
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-09 17:28:14 +0900

    감싸인 요소 2

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 
 function App() {
   return (
-    <h1>리액트 안녕!</h1>
-    <h2>잘 작동하니?</h2>
+    <div>
+      <h1>리액트 안녕!</h1>
+      <h2>잘 작동하니?</h2>
+    </div>
   );
 }
 
