fb7b5734cbe21e6be1adfcec6774b19d3784a596
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-09 17:49:58 +0900

    AND 연산자(&&)를 사용한 조건부 렌더링 2

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function App() {
   const name = "JavaScript";
-  return <div>{name === "React" ? <h1>리액트입니다.</h1> : null}</div>;
+  return <div>{name === "React" && <h1>리액트입니다.</h1>}</div>;
 }
 
 export default App;
