b0c912c12990c7866143b1b179d78170e527eb60
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-10 15:47:19 +0900

    class 대신 className

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,20 +3,7 @@ import "./App.css";
 
 function App() {
   const name = "React";
-  return (
-    <div
-      style={{
-        // background-color는 backgroundColor와 같이 - 가 사라지고 카멜 표기법으로 작성
-        backgroundColor: "black",
-        color: "aqua",
-        fontSize: 48,
-        fontWeight: "bold",
-        padding: 16,
-      }}
-    >
-      {name}
-    </div>
-  );
+  return <div className="react">{name}</div>;
 }
 
 export default App;
