e801bcbec49d6d50b0432125891dd5e4fd70fad6
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-10 16:41:41 +0900

    3장 컴포넌트 시작

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,18 +3,7 @@ import "./App.css";
 
 function App() {
   const name = "React";
-  return (
-    <>
-      {/* 주석은 이렇게 작성합니다. */}
-      <div
-        className="react" // 시작 태그를 여러 줄로 작성하게 된다면 여기에 주석을 작성할 수 있습니다.
-      >
-        {name}
-      </div>
-      // 하지만 이런 주석이나 /* 이런 주석은 페이지에 그대로 나가게 됩니다. */
-      <input />
-    </>
-  );
+  return <div className="react">{name}</div>;
 }
 
 export default App;
