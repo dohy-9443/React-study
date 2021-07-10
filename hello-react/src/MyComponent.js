b183181db05e8d461afa7f7c2796ac4f3190f50c
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-10 18:24:53 +0900

    isRequired를 사용하여 필수 propTypes 설정

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,11 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const MyComponent = ({ name, children }) => {
+const MyComponent = ({ name, favoriteNumber, children }) => {
   return (
     <div>
       <h1>안녕하세요! 제 이름은 {name} 입니다!</h1>
       <h2>children 값은 '{children}'입니다.</h2>
+      <h3>제가 좋아하는 숫자는 '{favoriteNumber}' 입니다.</h3>
     </div>
   );
 };
@@ -18,6 +19,7 @@ MyComponent.defaultProps = {
 // propTypes 를 통한 props 검증
 MyComponent.propTypes = {
   name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired,
 };
 
 export default MyComponent;
