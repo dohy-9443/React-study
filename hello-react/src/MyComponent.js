0d0d0511db56dd0cf8530012c2b3310579899ea8
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-10 17:40:49 +0900

    props 기본값 설정: defaultProps

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -4,4 +4,9 @@ const MyComponent = (props) => {
   return <h1>안녕하세요! 제 이름은 {props.name} 입니다!</h1>;
 };
 
+// defaultProps 값 설정하기
+MyComponent.defaultProps = {
+  name: "기본 이름",
+};
+
 export default MyComponent;
