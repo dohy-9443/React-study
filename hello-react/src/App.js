d7964a4c645ad6df7ba4fa1af02c196180e6849d
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-10 16:23:08 +0900

    꼭 닫아야하는 태그 self-closing

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,8 +3,12 @@ import "./App.css";
 
 function App() {
   const name = "React";
-  return <div className="react">{name}</div>;
-  <input></input>;
+  return (
+    <>
+      <div className="react">{name}</div>
+      <input />
+    </>
+  );
 }
 
 export default App;
