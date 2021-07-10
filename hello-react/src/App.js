e6533a69e50dabf883d4222994f02b4d96eb38c8
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-10 15:39:25 +0900

    인라인 스타일링 1

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,8 +2,16 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  const name = undefined;
-  return <div>{name || "리액트"}</div>;
+  const name = "React";
+  const style = {
+    // background-color는 backgroundColor와 같이 - 가 사라지고 카멜 표기법으로 작성
+    backgroundColor: "black",
+    color: "aqua",
+    fontSize: 48,
+    fontWeight: "bold",
+    padding: 16,
+  };
+  return <div style={style}>{name}</div>;
 }
 
 export default App;
