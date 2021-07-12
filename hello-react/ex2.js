09db0ba2165cc2701799063ee3d4dc7a2f896f8e
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-12 14:59:36 +0900

    ES6 문법

--- hello-react/ex2.js
+++ hello-react/ex2.js
@@ -1,6 +1,4 @@
-var numbers = [1, 2, 3, 4, 5];
-var processed = numbers.map(function (num) {
-  return num * num;
-});
+const numbers = [1, 2, 3, 4, 5];
+const processed = numbers.map((num) => num * num);
 
 console.log(processed);
