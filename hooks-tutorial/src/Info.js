00b04aa1b7f42bf3812cfcf391ad5434eb324734
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-14 17:38:26 +0900

    useState를 여러 번 사용하기 Info

---
+++ hooks-tutorial/src/Info.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const Info = () => {
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const onChangeNickname = (e) => {
+    setNickname(e.target.value);
+  };
+
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChangeName} />
+        &nbsp;&nbsp;
+        <input value={nickname} onChange={onChangeNickname} />
+      </div>
+      <div>
+        <h1>
+          <b>이름 : </b> {name}
+        </h1>
+        <h1>
+          <b>닉네임 : </b> {nickname}
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
