848ed05d931f36264e96110aaee8d1c6cc3e5cb3
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-18 16:53:20 +0900

    URL 파라미터 App.js

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
+import Profile from "./Profile";
 
 const App = () => {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">HOME</Link>
+        </li>
+        <li>
+          <Link to="/about">ABOUT</Link>
+        </li>
+        <li>
+          <Link to="/profile/donghyun">DongHyun의 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong의 프로필</Link>
+        </li>
+      </ul>
+
       <Route path="/" component={Home} exact={true} />
-      <Route path={["/About", "/Info"]} component={About} />
+      <Route path={["/about", "/info"]} component={About} />
+      <Route path="/profile/:username" component={Profile} />
     </div>
   );
 };
