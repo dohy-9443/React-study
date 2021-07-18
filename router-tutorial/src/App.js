23f0c3c09b1c442388c2fd132f0798045d0e9b74
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-18 16:14:34 +0900

    Route 하나에 여러 개의 path 설정하기

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,22 +1,13 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 
 const App = () => {
   return (
     <div>
-      <ul>
-        <li>
-          <Link to="/">HOME</Link>
-        </li>
-        <li>
-          <Link to="/About">ABOUT</Link>
-        </li>
-      </ul>
-      <hr />
       <Route path="/" component={Home} exact={true} />
-      <Route path="/About" component={About} />
+      <Route path={["/About", "/Info"]} component={About} />
     </div>
   );
 };
