8e37a1d6b6900e3c8329915cae6ca1a30a788085
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-20 17:31:46 +0900

    App 렌더링

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ColorBox from "./components/ColorBox";
 import { ColorProvider } from "./contexts/color";
-import SelectColors from "./contexts/SelectColors";
+import SelectColors from "./components/SelectColors";
 
 const App = () => {
   return (
