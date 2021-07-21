f724b06affd1fef124b22a70fcf27df16f2c8e52
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-21 14:44:25 +0900

    리듀서 함수 정의

--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -8,10 +8,37 @@ const INCREASE = "INCREASE";
 const DECREASE = "DECREASE";
 
 const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
-const increase = (defference = { type: INCREASE, defference });
+const increase = (defference) => ({
+  type: INCREASE,
+  defference,
+});
 const decrease = () => ({ type: DECREASE });
 
 const initialState = {
   toggle: false,
   counter: 0,
 };
+
+// state가 undefined일 때는 initialState를 기본값으로 사용
+function reducer(state = initialState, action) {
+  // action.type에 따라 다른 작업을 처리
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state, // 불변성 유지
+        toggle: !state.toggle,
+      };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + action.defference,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    default:
+      return false;
+  }
+}
