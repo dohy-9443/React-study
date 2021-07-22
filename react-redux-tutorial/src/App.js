d4fa32cf039a76bc51aee09c3d480103612aa292
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-22 18:19:19 +0900

    Todos 컴포넌트를 TodosContainer 컴포넌트로 교체

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Todos from './components/Todos';
 import CounterContainer from './containers/CounterContainer';
+import TodosConatiner from './containers/TodosConatiner';
 
 const App = () => {
   return (
     <div>
       <CounterContainer />
       <hr />
-      <Todos />
+      <TodosConatiner />
     </div>
   );
 };
