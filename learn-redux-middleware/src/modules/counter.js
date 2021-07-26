ccfcda9f06f409c4853d28c8a749701852bc0874
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-26 18:31:18 +0900

    비동기 카운터

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -1,22 +1,35 @@
 import { createAction, handleActions } from "redux-actions";
+import { delay, put, takeEvery, takeLatest } from "react-saga/effects";
 
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
 
+const INCREMENT_ASYNC = "counter/INCREMENT_ASYNC";
+const DECREMENT_ASYNC = "counter/DECREMENT_ASYNC";
+
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
-// 1초 뒤에 increase 혹은 decrease 함수를 디스패치함
-export const increaseAsync = () => (dispatch) => {
-  setTimeout(() => {
-    dispatch(increase());
-  }, 1000);
-};
-export const decreaseAsync = () => (dispatch) => {
-  setTimeout(() => {
-    dispatch(decrease());
-  }, 1000);
-};
+export const increaseAsync = createAction(INCREMENT_ASYNC, () => undefined);
+export const decreaseAsync = createAction(DECREMENT_ASYNC, () => undefined);
+
+function* increaseSaga() {
+  yield delay(1000); // 1초를 기다린다.
+  yield put(increase()); // 특정 액션을 디스패치한다.
+}
+
+function* decreaseSaga() {
+  yield delay(1000); // 1초를 기다린다.
+  yield put(decrease()); // 특정 액션을 디스패치한다.
+}
+
+export function* counterSaga() {
+  // takeEvery는 들어오는 모든 액션에 대해 특정 작업을 처리해준다.
+  yield takeEvery(INCREMENT_ASYNC, increaseAsync);
+  // takeLatest는 기존에 진행 중이던 작업이 있다면 취소 처리하고
+  // 가장 마지막으로 실행된 작업만 수행한다.
+  yield takeLatest(DECREMENT_ASYNC, decreaseAsync);
+}
 
 const initialState = 0; // 상태는 꼭 객체일 필요가 없다. 숫자도 작동
 
