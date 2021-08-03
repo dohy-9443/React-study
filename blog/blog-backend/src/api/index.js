932c18a6cadfba25b0ec8f6a4519a4afb78576b5
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-03 19:03:43 +0900

    posts 라우트 생성 api 라우트에 posts 라우트를 연결

--- blog/blog-backend/src/api/index.js
+++ blog/blog-backend/src/api/index.js
@@ -1,9 +1,9 @@
 const Router = require('koa-router');
+const posts = require('./posts');
+
 const api = new Router();
 
-api.get('/test', (ctx) => {
-  ctx.body = `<h1>text 성공</h1>`;
-});
+api.use('/posts', posts.routes());
 
 // 라우터를 내보냄
 module.exports = api;
