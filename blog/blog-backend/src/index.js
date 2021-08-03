3858bf0c78f65a758b0311cd151141f7936628d8
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-03 18:46:39 +0900

    라우트 모듈화

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,27 +1,13 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 
+const api = require('./api');
+
 const app = new Koa();
 const router = new Router();
 
 // 라우터 설정
-router.get('/', (ctx) => {
-  ctx.body = `<h1>HOME</h1>`;
-});
-
-router.get('/about/:name?', (ctx) => {
-  const { name } = ctx.params;
-  // name의 존재 유무에 따라 다른 결과 출력
-  ctx.body = name ? `<h1>${name}의 소개</h1>` : `<h1>소개</h1>`;
-});
-
-router.get('/posts', (ctx) => {
-  const { id } = ctx.query;
-  // id의 존재 유무에 따라 다른 결과 출력
-  ctx.body = id
-    ? `<h1>포스트 #${id}</h1>`
-    : '<h1>포스트 아이디가 없습니다.</h1>';
-});
+router.use('/api', api.routes()); // api 라우트 적용
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
