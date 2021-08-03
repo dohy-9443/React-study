313d00b8af3f2f6fab0b7a8aceb994786a234601
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-03 17:49:21 +0900

    미들웨어 제거

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,26 +1,6 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(async (ctx, next) => {
-  console.log(ctx.url);
-  console.log(1);
-  if (ctx.query.authorized != '1') {
-    ctx.state = 401; // Unauthorized
-    return;
-  }
-  await next();
-  console.log('END');
-});
-
-app.use((ctx, next) => {
-  console.log(2);
-  next();
-});
-
-app.use((ctx) => {
-  ctx.body = 'hello world';
-});
-
 app.listen(4000, () => {
   console.log('Listening to port 4000');
 });
