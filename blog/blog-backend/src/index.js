2a96bab9afa2d61c831be450378e4821ac19511a
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-03 18:17:55 +0900

    기본 사용법

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,5 +1,19 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
 const app = new Koa();
+const router = new Router();
+
+// 라우터 설정
+router.get('/', (ctx) => {
+  ctx.body = `<h1>HOME</h1>`;
+});
+router.get('/about', (ctx) => {
+  ctx.body = `<h1>ABOUT</h1>`;
+});
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
   console.log('Listening to port 4000');
