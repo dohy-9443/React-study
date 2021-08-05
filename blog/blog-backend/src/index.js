04cf681da040a1639c285cd7c6597472e7cf188d
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-05 16:13:08 +0900

    mongoose로 서버와 데이터베이스 연결

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -4,10 +4,21 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 
-// 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
-const { PORT } = process.env;
+const mongoose = require('mongoose');
 
 const api = require('./api');
+
+// 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
 
 const app = new Koa();
 const router = new Router();
@@ -24,5 +35,5 @@ app.use(router.routes()).use(router.allowedMethods());
 // PORT가 지정되어 있지 않다면 4000을 사용
 const port = PORT || 4000;
 app.listen(port, () => {
-  console.log('Listening to port %d'.port);
+  console.log(`Listening to port %d`, port);
 });
