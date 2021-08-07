da9711d6fe9a149901d8b174ef97cfcf9090e3fc
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-07 18:40:07 +0900

    토큰 재발급하기

--- blog/blog-backend/src/lib/jwtMiddleware.js
+++ blog/blog-backend/src/lib/jwtMiddleware.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
+import User from '../models/user';
 
-const jwtMiddleware = (ctx, next) => {
+const jwtMiddleware = async (ctx, next) => {
   const token = ctx.cookies.get('access_token');
 
   if (!token) {
@@ -13,10 +14,21 @@ const jwtMiddleware = (ctx, next) => {
       _id: decoded._id,
       username: decoded.username,
     };
+    // 토큰 3.5일 미만 남으면 재발급
+    const now = Math.floor(Date.now() / 1000);
+    if (decoded.exp - now < 60 * 60 * 24 * 3.5) {
+      const user = await User.findById(decoded._id);
+      const token = user.generateToken();
+      ctx.cookies.set('access_token', token, {
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+        httpOnly: true,
+      });
+    }
     console.log(decoded);
     return next();
   } catch (e) {
     // 토큰 검증 실패
+    console.log('실패..');
     return next();
   }
 };
