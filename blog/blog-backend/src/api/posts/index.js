41f67dfd5e0b7ec2e3b8588bd2f602b12f7dec55
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-07 19:00:48 +0900

    posts 라우터에서 사용

--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -1,15 +1,16 @@
 import Router from 'koa-router';
 import * as postsCtrl from './posts.ctrl';
+import checkLoggedIn from '../../lib/checkLoggenIn';
 
 const posts = new Router();
 
 posts.get('/', postsCtrl.list);
-posts.post('/', postsCtrl.write);
+posts.post('/', checkLoggedIn, postsCtrl.write);
 
 const post = new Router(); // /api/posts/:id
 post.get('/:id', postsCtrl.read);
-post.delete('/:id', postsCtrl.remove);
-post.patch('/:id', postsCtrl.update);
+post.delete('/:id', checkLoggedIn, postsCtrl.remove);
+post.patch('/:id', checkLoggedIn, postsCtrl.update);
 
 posts.use('/:id', postsCtrl.checkObjectId, post.routes());
 
