1b3469c2fbdc7d35d8c793e6bad9554e2dd7e642
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-09 18:13:57 +0900

    AuthForm에 Button을 사용하는 부분을 수정

--- blog/blog-frontend/src/components/auth/AuthForm.js
+++ blog/blog-frontend/src/components/auth/AuthForm.js
@@ -44,6 +44,10 @@ const Footer = styled.div`
   }
 `;
 
+const ButtonWithMarginTop = styled(Button)`
+  margin-top: 1rem;
+`;
+
 const AuthForm = () => {
   return (
     <AuthFormBlock>
@@ -56,7 +60,7 @@ const AuthForm = () => {
           placeholder="비밀번호"
           type="password"
         />
-        <Button>로그인</Button>
+        <ButtonWithMarginTop cyan fullWidth>로그인</ButtonWithMarginTop>
       </form>
       <Footer>
         <Link to="/register">회원가입</Link>
