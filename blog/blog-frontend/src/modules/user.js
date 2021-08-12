97303ac4d2574b9e77a11b48adf0e02c092ee9b4
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-12 19:13:08 +0900

    오타 수정 및 Header 컴포넌트에서 로그아웃 버튼 누르면 해당 함수가 호출되도록 구현

--- blog/blog-frontend/src/modules/user.js
+++ blog/blog-frontend/src/modules/user.js
@@ -2,15 +2,14 @@ import { createAction, handleActions } from 'redux-actions';
 import { takeLatest, call } from 'redux-saga/effects';
 import * as authAPI from '../lib/api/auth';
 import createRequestSaga, {
-  createRequestActionTypes
+  createRequestActionTypes,
 } from '../lib/createRequestSaga';
 
 const TEMP_SET_USER = 'user/TEMP_SET_USER'; // 새로고침 이후 임시 로그인 처리
 // 회원 정보 확인
 const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] = createRequestActionTypes(
-  'user/CHECK'
+  'user/CHECK',
 );
-
 const LOGOUT = 'user/LOGOUT';
 
 export const tempSetUser = createAction(TEMP_SET_USER, user => user);
@@ -21,7 +20,7 @@ const checkSaga = createRequestSaga(CHECK, authAPI.check);
 
 function checkFailureSaga() {
   try {
-    localStorage.removeItem('user'); // localStorage에서 user를 제거
+    localStorage.removeItem('user'); // localStorage 에서 user 제거하고
   } catch (e) {
     console.log('localStorage is not working');
   }
@@ -30,7 +29,7 @@ function checkFailureSaga() {
 function* logoutSaga() {
   try {
     yield call(authAPI.logout); // logout API 호출
-    localStorage.removeItem('user'); // localStorage에서 user를 제거
+    localStorage.removeItem('user'); // localStorage 에서 user 제거
   } catch (e) {
     console.log(e);
   }
@@ -44,29 +43,29 @@ export function* userSaga() {
 
 const initialState = {
   user: null,
-  checkError: null
+  checkError: null,
 };
 
 export default handleActions(
   {
     [TEMP_SET_USER]: (state, { payload: user }) => ({
       ...state,
-      user
+      user,
     }),
     [CHECK_SUCCESS]: (state, { payload: user }) => ({
       ...state,
       user,
-      checkError: null
+      checkError: null,
     }),
     [CHECK_FAILURE]: (state, { payload: error }) => ({
       ...state,
       user: null,
-      checkError: error
+      checkError: error,
     }),
     [LOGOUT]: state => ({
       ...state,
       user: null,
-    })
+    }),
   },
-  initialState
+  initialState,
 );
